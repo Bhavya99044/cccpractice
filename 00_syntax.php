8d09211379c5bad8cc9c24c7b75cd8120706051b
cipher <bhavya99044@gmail.com>
2024-01-17 16:36:54 +0530

    str function

--- 00_syntax.php
+++ 00_syntax.php
@@ -1,31 +1,45 @@
-
-
-
 <?php
-if(isset($_GET['keyword'])){
 
-$keyword=$_GET['keyword'];
-echo $keyword,'<br>';
+$str="my name is,";
+$length=strlen($str);
+echo "string length is $length"."<br>";
+
+
+//replace
+
+$message="my name is bhavya ";
+
+$replaceMessage=str_replace("bhavya","arun",$message);
+
+echo $replaceMessage;
+//pso
+
+$org="this is me";
+
+$variable="me";
+
+$position=strpos($org,$variable);
+
+echo "$position"."<br>";
+
+//substr
+
+$subString=substr($message,2,3);
+echo "$subString<br>";
+
+$trim=trim($message);
+echo "$trim<br>";
+function fun($var){
+    echo("the new $var");
 }
+$language=array("java","js","php");
+
+$implo =implode("*",$language);
+fun($implo);
+
+
+$explo=explode(" ",$message);
+fun($explo);
+
+
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-
-
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="get">
-<input type="text" name="keyword" placeholder="type here">
-<button>search</button>
-
-</form>
-<!--Echo HTML tags-->
-</body>
-</html>
